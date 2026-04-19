666d3c9722fa4e751d781daa9a59286f6d6fae35
Austin Watson <austingwatson@hotmail.com>
2026-04-19 00:09:36 -0500

    added test owen screen

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -7,3 +7,7 @@ func _on_play_button_pressed() -> void:
 
 func _on_test_level_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/test_level_screen.tscn")
+
+
+func _on_owen_test_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/test_owen_screen.tscn")
