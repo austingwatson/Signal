82b84ffec6adba652cf02e7cbdcbf4ab3db5982e
Austin Watson <austingwatson@hotmail.com>
2026-04-23 21:03:25 -0500

    added pickup sound

--- scripts/component/pickup_component.gd
+++ scripts/component/pickup_component.gd
@@ -3,8 +3,15 @@ extends Area2D
 
 @export var parent: Node
 @export var material_amount: MaterialAmount
+@onready var pickup_sound := $PickupSound
 
 func _on_area_entered(area: Area2D) -> void:
 	GlobalSignals.call_added_metal(material_amount.metal_amount)
 	GlobalSignals.call_added_power_cell(material_amount.power_cell_amount)
+	pickup_sound.play()
+	area_entered.disconnect(_on_area_entered)
+	parent.visible = false
+
+
+func _on_pickup_sound_finished() -> void:
 	parent.queue_free()
