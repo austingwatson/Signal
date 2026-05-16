21dac279aa7bafc4b05bde585aee2e33dc6d4fba
Owesomewilser <owesomewilser@gmail.com>
2026-05-16 15:05:19 -0400

    Postjam 1.5
    Added Titlescreen and PLAY button function. Fixed heavy bot animation. Added font.

--- scripts/screen/title_screen.gd
+++ scripts/screen/title_screen.gd
@@ -1,13 +1,25 @@
 extends Node
 
+@onready var animation_player := $AnimationPlayer
 
-func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/screen/game_screen.tscn")
 
 
 func _on_test_level_button_pressed() -> void:
+	$ButtonClick.play()
 	get_tree().change_scene_to_file("res://scenes/screen/test_level_screen.tscn")
 
 
-func _on_owen_test_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/screen/test_owen_screen.tscn")
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/screen/game_screen.tscn")
+
+
+func _on_guide_pressed() -> void:
+	$ButtonClick.play()
+
+
+func _on_play_mouse_entered() -> void:
+	$ButtonHover.play()
+
+
+func _on_guide_mouse_entered() -> void:
+	$ButtonHover.play()
